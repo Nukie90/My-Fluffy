c4a1d95e8fe6b4ef1e075facea9951af330149fb
b1ngoinho11 <budthbundit.thayaurai@gmail.com>
2024-10-06 19:33:35 +0700

    added api fetch to Saved

--- frontend/src/Pages/Profile/Profile.jsx
+++ frontend/src/Pages/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import Posts from '../../Components/Posts';
 import axios from 'axios';
 
 
-function Profile({ currentPage, setCurrentPage }) {
+function Profile({ setCurrentPage }) {
     const [isLogged, setIsLogged] = useState(false);
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -52,7 +52,7 @@ function Profile({ currentPage, setCurrentPage }) {
     }, [setCurrentPage]);
 
     return (
-        <div className='h-screen w-full bg-red-200 flex flex-col justify-center items-center'>
+        <div className='h-screen w-full bg-red-200 flex flex-col'>
             {isLogged ? (
                 <div>
                     <h1>Profile</h1>
@@ -63,7 +63,7 @@ function Profile({ currentPage, setCurrentPage }) {
                     <div className="flex sm:flex-col md:flex-row h-auto w-full pt-20 sm:px-4 md:px-6 lg:px-8">
                         <div className='h-auto pt-4 pb-20'>
                             <div className='h-auto'>
-                            <h1 className='sm:text-xl md:text-2xl font-bold'>Saved Posts</h1>
+                            <h1 className='sm:text-xl md:text-2xl font-bold'>Your Posts</h1>
                             </div>
 
                             {loading && <p>Loading posts...</p>}
--- frontend/src/Pages/Saved/Saved.jsx
+++ frontend/src/Pages/Saved/Saved.jsx
@@ -1,15 +1,85 @@
+import { Link } from 'react-router-dom';
 import './../../App.css';
+import Posts from '../../Components/Posts';
+import axios from 'axios';
 
 import { useState, useEffect } from 'react'; 
 
 function Saved({currentPage, setCurrentPage}) {
+    const [isLogged, setIsLogged] = useState(false);
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchPosts = async () => {
+
+        setLoading(true);
+        try {
+          const response = await axios.get(`http://localhost:3000/api/v1/saved_posts/`,
+            {
+                withCredentials: true,
+            }
+          );
+          const newPosts = response.data; 
+          console.log('Fetched posts:', newPosts);
+    
+        const formattedPosts = newPosts.map(post => ({
+            id: post.id,
+            title: post.title,
+            content: post.content,
+            status: post.status,
+            picture: post.picture,
+            reward: post.reward
+        }));
+
+        setPosts(prevPosts => [...prevPosts, ...formattedPosts]); // Append new posts
+          
+        } catch (error) {
+          console.error('Error fetching posts:', error);
+        } finally {
+          setLoading(false);
+        }
+    };
+
     useEffect(() => {
         setCurrentPage('Saved');
+        const cookies = document.cookie.split('; ');
+        const sessionCookie = cookies.find(cookie => cookie.startsWith('session='));
+        
+        if (sessionCookie) {
+            setIsLogged(true);
+            fetchPosts();
+        }
     }, [setCurrentPage]);
     
     return (
-        <div className='h-auto bg-red-200'>
-            this is the Saved page
+        <div className='h-screen w-full bg-red-200 flex flex-col justify-center items-center'>
+            {isLogged ? (
+                <div>
+                    <div className="flex sm:flex-col md:flex-row h-auto w-full pt-20 sm:px-4 md:px-6 lg:px-8">
+                        <div className='h-auto pt-4 pb-20'>
+                            <div className='h-auto'>
+                            <h1 className='sm:text-xl md:text-2xl font-bold'>Saved Posts</h1>
+                            </div>
+
+                            {loading && <p>Loading posts...</p>}
+
+                            <Posts data={posts} />
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <div className='w-full h-full flex flex-col justify-center items-center bgct-green'>
+                    <p>Please log in to saved posts</p>
+                    <Link to='/login'>
+                        <button 
+                            className='h-10 w-24 my-4 flex justify-center items-center bgct-yellow rounded-lg'
+                            onClick={() => setCurrentPage('Login')}
+                        >
+                            Log In
+                        </button>
+                    </Link>
+                </div>
+            )}
         </div>
     );
 }
